0a40fb5b07f346f5c3de7e1390ae4746bc6230f7
francklinsubia <francklin.subia@upeu.edu.pe>
2021-10-24 17:56:10 +0000

    Modificacion 2

---
+++ ReforzamientoUpeUUG1/src/main/java/pe/edu/upeu/App.java
@@ -0,0 +1,17 @@
+package pe.edu.upeu;
+
+import pe.edu.BasicoJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+   BasicoJava bj=new BasicoJava();
+   bj.tablamultiplicar();
+    }
+}
