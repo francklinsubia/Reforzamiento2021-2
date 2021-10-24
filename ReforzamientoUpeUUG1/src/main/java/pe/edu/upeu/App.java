c28a64288b9e4d86b325d26a43352d42545da7bf
francklinsubia <francklin.subia@upeu.edu.pe>
2021-10-24 22:37:37 +0000

    Opereaciones

--- ReforzamientoUpeUUG1/src/main/java/pe/edu/upeu/App.java
+++ ReforzamientoUpeUUG1/src/main/java/pe/edu/upeu/App.java
@@ -114,5 +114,15 @@ public class App{
         operadores();
         //operadores();
         controlStructure();
+        tablamultiplicar();
     }
+    public static void tablamultiplicar() {
+        for (int i = 1; i <=10; i++) {
+        for (int j = 1; j <=10; j++) {
+         System.out.println(i+" * "+j+" = "+(i*j));   
+        }
+        System.out.println();            
+    }
+        }   
+
 }
