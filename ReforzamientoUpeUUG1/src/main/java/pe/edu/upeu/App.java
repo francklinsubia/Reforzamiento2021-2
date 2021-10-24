63ebc59f4cf0072edfa801060efca1085953f458
francklinsubia <francklin.subia@upeu.edu.pe>
2021-10-24 21:02:12 +0000

    Reforzamientog2

--- ReforzamientoUpeUUG1/src/main/java/pe/edu/upeu/App.java
+++ ReforzamientoUpeUUG1/src/main/java/pe/edu/upeu/App.java
@@ -1,17 +1,43 @@
 package pe.edu.upeu;
 
-import pe.edu.BasicoJava;
+import java.util.Date;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-   BasicoJava bj=new BasicoJava();
-   bj.tablamultiplicar();
+public class App{
+
+    public static void tiposdatos(){
+        byte a=1;
+        short b=8;
+        int c=87;
+        float d=84.5f;
+        double f=87.21;
+
+        boolean e=true; 
+
+        char h='+';
+
+        String g=" hola";
+        Integer xx;
+        Double xxx;
+        Date ss;
+        
     }
+
+    public static void operadores() {
+        
+    }
+
+    public static void controlStructure() {
+        
+    }    
+
+    
+
+
+    public static void main( String[] args )
+        System.out.println( "Hello World!" );
+}
 }
