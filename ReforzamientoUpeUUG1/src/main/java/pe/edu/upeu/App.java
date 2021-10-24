bf019e20723f525f0f976b34e3c78053c10940e1
francklinsubia <francklin.subia@upeu.edu.pe>
2021-10-24 22:00:07 +0000

    operadores de asignacion

--- ReforzamientoUpeUUG1/src/main/java/pe/edu/upeu/App.java
+++ ReforzamientoUpeUUG1/src/main/java/pe/edu/upeu/App.java
@@ -1,52 +1,70 @@
 package pe.edu.upeu;
-
 import java.util.Date;
-
 /**
  * Hello world!
  *
  */
 public class App{
-
     public static void tiposdatos(){
         byte a=1;
         short b=8;
         int c=87;
         float d=84.5f;
         double f=87.21;
-
         boolean e=true; 
-
         char h='+';
-
         String g=" hola";
         Integer xx;
         Double xxx;
         Date ss;
         
+
     }
 
     public static void operadores() {
-        int a=7;
-        int b=2;
-        //operadores aritmeticos
-    System.out.println("suma:"+(a+b));
-    System.out.println("resta:"+(a-b));
-    System.out.println("division:"+(a/b));
-    System.out.println("multiplicacion:"+(a*b));
-    System.out.println("modulo:"+(a%b));
-    System.out.println("Potencia:"+(Math.pow(a, b)));
 
+       int a=7;
+       int b=2;
+       //operadores aritmeticos
+       System.out.println("suma:"+(a+b)); 
+       System.out.println("resta:"+(a-b)); 
+       System.out.println("division:"+(a/b)); 
+       System.out.println("multiplicacion:"+(a*b)); 
+       System.out.println("modulo:"+(a%b)); 
+       System.out.println("Potencia:"+(Math.pow(a, b)));
+        //operadores logicos
+        int ologx=(a>b && a!=b )?1:0;
+        System.out.println(ologx);
+        int ologxx=(a>b || a==b )?1:0;
+        System.out.println(ologxx);
+        int ologxxx=!(a>b || a==b )?1:0;
+        System.out.println(ologxxx);
+        //operadores relacioales
+        System.out.println((a>b)?"verdadero":"falso");
+        System.out.println((a>=b)?"verdadero":"falso");
+        System.out.println((a<b)?"verdadero":"falso");
+        System.out.println((a==b)?"verdadero":"falso");
+        System.out.println((a!=b)?"verdadero":"falso");
+        //adcionales
+        int x=4;
+        System.out.println(--x);//3
+        System.out.println(x++);//3 ---4
+        System.out.println(x--);//4 ---3
+        System.out.println(x+=2);//5 
+        System.out.println(x-=2);//3
+        System.out.println(x*=2);//6
+        System.out.println(x/=2);//3
+        System.out.println(x%=2);//1
     }
 
     public static void controlStructure() {
-        
-    }    
 
-    
+    }
 
+    public static void main( String[] args ){
 
-    public static void main( String[] args )
         System.out.println( "Hello World!" );
-}
+        //System.out.println( "Hello World!" );
+        operadores();
+    }
 }
